9a5416fa7a516c147ea8e530fa11d80dd6de1c3f
ReisMiner <r1miner@outlook.com>
2022-12-01 14:40:50 +0100

    task 2 day 2 almost done
    Signed-off-by: ReisMiner <r1miner@outlook.com>

--- aoc/Day 01/Program.cs
+++ aoc/Day 01/Program.cs
@@ -8,15 +8,16 @@ class Program
     static void Main(string[] args)
     {
         List<string> lines = FileHelper.ReadInputFile(args);
+        const int getTopCount = 3;
 
-        int max = 0, current = 0;
-
+        List<int> maxes = new();
+        int current = 0;
+        
         foreach (var line in lines)
         {
             if (line == "")
             {
-                if (current > max)
-                    max = current;
+                maxes.Add(current);
                 current = 0;
             }
             else
@@ -24,7 +25,11 @@ class Program
                 current += Convert.ToInt32(line);
             }
         }
-
-        Console.WriteLine(max);
+        
+        maxes.Sort();
+        maxes.Reverse();
+        Console.WriteLine();
     }
+    
+    
 }
