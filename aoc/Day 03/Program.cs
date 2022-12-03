1d435a27ea2991219648dbcbae9aa9ae2f343bc0
ReisMiner <r1miner@outlook.com>
2022-12-03 11:33:35 +0100

    task 1 of day 3 done

---
+++ aoc/Day 03/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using solveHelpers;
+
+namespace Day_03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> lines = FileHelper.ReadInputFile(args);
+        int total = 0;
+
+        List<char> letters = new();
+
+        //grab letters
+        foreach (var line in lines)
+        {
+            string first = line.Substring(0, line.Length / 2);
+            string second = line.Substring(line.Length / 2, line.Length / 2);
+
+            foreach (var letter in first)
+            {
+                if (second.Contains(letter))
+                {
+                    letters.Add(letter);
+                    break;
+                }
+            }
+        }
+
+        foreach (char c in letters)
+        {
+            int letter = Convert.ToInt32(c);
+
+            if (letter < 96)
+            {
+                total += letter - 38;
+            }
+            else
+            {
+                total += letter - 96;
+            }
+        }
+        Console.WriteLine(total);
+    }
+}
