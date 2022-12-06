ff90c9fd8bd8610f6a71cca940a5f7687f0f585d
Ramon Camenzind <r1miner@outlook.com>
2022-12-06 11:04:45 +0100

    day 6 task 1&2 done. for task 1 just change le variablé to 4 :)

---
+++ aoc/Day 06/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using solveHelpers;
+
+namespace Day_06;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> lines = FileHelper.ReadInputFile(args);
+        
+        List<Char> letters = new();
+
+        string line = lines[0];
+        int startSize = 14;
+        
+        for (int i = 0; i < line.Length; i++)
+        {
+            letters.Add(line[i]);
+            if (i >= startSize)
+            {
+                letters.RemoveAt(0);
+            }
+
+            if (letters.Count >= startSize)
+            {
+                if (letters.Distinct().Count() == startSize)
+                {
+                    Console.WriteLine(i+1);
+                    break;
+                }
+            }
+        }
+    }
+}
