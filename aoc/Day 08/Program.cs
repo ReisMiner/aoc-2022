d6fdd26ad6dec695fadef1970cd0a162fd521e0a
ReisMiner <r1miner@outlook.com>
2022-12-08 17:38:38 +0100

    :fire: day 8 task 2 done

--- aoc/Day 08/Program.cs
+++ aoc/Day 08/Program.cs
@@ -2,25 +2,116 @@
 
 namespace Day_08;
 
-//1791 too low
-//1812 too low
-
 class Program
 {
     static void Main(string[] args)
     {
         List<string> lines = FileHelper.ReadInputFile(args);
-        List<List<int>> grid = new();
+        List<List<int>> grid = Setup(lines);
 
-        for (int i = 0; i < lines.Count; i++)
+        // int visibleTrees = Task1(grid);
+        // Console.WriteLine(visibleTrees);
+
+        int bestDistance = Task2(grid);
+        Console.WriteLine(bestDistance);
+    }
+
+    static int Task2(List<List<int>> grid)
+    {
+        int bestDistance = 0;
+        for (int i = 0; i < grid.Count; i++)
         {
-            grid.Add(new List<int>());
-            for (int j = 0; j < lines[i].Length; j++)
+            if (i == grid.Count - 1 || i == 0)
             {
-                grid[i].Add(Convert.ToInt32(lines[i][j].ToString()));
+                continue;
+            }
+
+            for (int j = 0; j < grid[i].Count; j++)
+            {
+                if (j == grid[i].Count - 1 || j == 0)
+                {
+                    continue;
+                }
+
+                //CHECKING BELOW
+
+                int treeValue = grid[i][j];
+                List<int> distances = new() { 0, 0, 0, 0 };
+                // check right
+                for (int x = j + 1; x < grid[i].Count; x++)
+                {
+                    if (grid[i][x] >= treeValue)
+                    {
+                        distances[0]++;
+                        break;
+                    }
+
+                    if (grid[i][x] < treeValue)
+                    {
+                        distances[0]++;
+                    }
+                }
+
+                // check left
+                for (int x = j - 1; x >= 0; x--)
+                {
+                    if (grid[i][x] >= treeValue)
+                    {
+                        distances[1]++;
+                        break;
+                    }
+
+                    if (grid[i][x] < treeValue)
+                    {
+                        distances[1]++;
+                    }
+                }
+
+                // check up
+                for (int y = i - 1; y >= 0; y--)
+                {
+                    if (grid[y][j] >= treeValue)
+                    {
+                        distances[2]++;
+                        break;
+                    }
+
+                    if (grid[y][j] < treeValue)
+                    {
+                        distances[2]++;
+                    }
+                }
+
+                // check down
+                for (int y = i + 1; y < grid.Count; y++)
+                {
+                    if (grid[y][j] >= treeValue)
+                    {
+                        distances[3]++;
+                        break;
+                    }
+
+                    if (grid[y][j] < treeValue)
+                    {
+                        distances[3]++;
+                    }
+                }
+
+                int res = distances[0] * distances[1] * distances[2] * distances[3];
+
+                if (res > bestDistance)
+                {
+                    bestDistance = res;
+                    Console.WriteLine($"new res {res}");
+                }
             }
         }
 
+        return bestDistance;
+    }
+
+    static int Task1(List<List<int>> grid)
+    {
         int visibleTrees = 0;
         for (int i = 0; i < grid.Count; i++)
         {
@@ -133,6 +224,21 @@ class Program
             }
         }
 
-        Console.WriteLine(visibleTrees);
+        return visibleTrees;
+    }
+
+    static List<List<int>> Setup(List<string> lines)
+    {
+        List<List<int>> grid = new();
+        for (int i = 0; i < lines.Count; i++)
+        {
+            grid.Add(new List<int>());
+            for (int j = 0; j < lines[i].Length; j++)
+            {
+                grid[i].Add(Convert.ToInt32(lines[i][j].ToString()));
+            }
+        }
+
+        return grid;
     }
 }
